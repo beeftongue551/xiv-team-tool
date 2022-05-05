097b009f8d97e0c7eb14df3b9875ac901fe8218f
辻忠頼 <tsuji@rasis-soft.co.jp>
2022-05-06 06:09:56 +0900

    アイテムリストにIL表示機能の追加

--- src/components/item/ItemList.vue
+++ src/components/item/ItemList.vue
@@ -4,8 +4,12 @@
       <v-expansion-panel
         v-for="itemData in itemsData"
         :key="itemData.id"
-        :title="itemData.name"
       >
+        <v-expansion-panel-title>
+          {{itemData.name}}
+          <v-spacer />
+          IL: {{itemData.itemLevel}}
+        </v-expansion-panel-title>
         <v-expansion-panel-text>
           <v-container>
             <v-row no-gutters>
