5dcf5a9dc76efeb1da6e8eed30e6dd4d137adb1e
辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>
2022-05-06 07:55:44 +0900

    CharacterViewのCompositionAPI対応

--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -11,10 +11,11 @@
 import CharacterSearch from "@/components/character/CharacterSearch";
 import CharacterList from "@/components/character/CharacterList";
 import CharacterModal from "@/components/character/CharacterModal";
-export default {
+import {defineComponent} from "vue/dist/vue";
+export default defineComponent ({
   name: "CharacterView",
   components: {CharacterList, CharacterSearch, CharacterModal}
-}
+})
 </script>
 
 <style scoped>
