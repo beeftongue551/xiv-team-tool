d03734c2a43e61fd53a0d848f92bf44ae8a46c12
辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>
2022-05-10 11:32:54 +0900

    ナビゲーションドロワーの追加
    Signed-off-by: 辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,18 +2,37 @@
   <v-app-bar
       color="primary"
   >
+    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <v-toolbar-title>XIVTeamTool</v-toolbar-title>
     <v-spacer />
     <EorzeaTime />
     <v-btn icon="mdi-home" to="/" />
   </v-app-bar>
+
+  <v-navigation-drawer
+    v-model="drawer"
+    bottom
+  >
+    <v-list-item prepend-icon="mdi-home" title="Home" value="Home" to="/" />
+    <v-divider />
+    <v-list-item prepend-icon="mdi-account-search" title="キャラ検索" value="キャラ検索" to="/character" />
+    <v-list-item prepend-icon="mdi-store" title="アイテム検索" value="アイテム検索" to="/item"/>
+  </v-navigation-drawer>
 </template>
 
 <script>
+import {ref} from "vue";
 import EorzeaTime from "@/components/EorzeaTime";
 export default {
   name: "NavBar",
-  components: {EorzeaTime}
+  components: {EorzeaTime},
+  setup() {
+    const drawer = ref(false)
+
+    return{
+      drawer
+    }
+  }
 }
 </script>
 
