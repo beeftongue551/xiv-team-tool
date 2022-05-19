f18cf2323a760328a0d2d6f81dcc87a9a96b82f9
辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>
2022-05-19 10:08:51 +0900

    スケジュール管理ボタンコンポーネントにスケジュール編集ボタンの追加
    Signed-off-by: 辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>

--- src/components/schedule/ScheduleActionButton.vue
+++ src/components/schedule/ScheduleActionButton.vue
@@ -1,10 +1,19 @@
 <template>
-  <v-btn color="warning" to="/team/schedule/edit">
-    <v-icon>
-      mdi-calendar
-    </v-icon>
-    スケジュール入力
-  </v-btn>
+  <div class="d-flex justify-space-around">
+    <v-btn color="warning" to="/team/schedule/edit">
+      <v-icon>
+        mdi-calendar
+      </v-icon>
+      スケジュール入力
+    </v-btn>
+
+    <v-btn color="secondary" to="/team/schedule/delete">
+      <v-icon>
+        mdi-calendar
+      </v-icon>
+      スケジュール編集
+    </v-btn>
+  </div>
 </template>
 
 <script>
