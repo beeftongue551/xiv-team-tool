637369a7f33d47ede53d98ad94e2fe7273636c53
辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>
2022-05-20 17:15:04 +0900

    表示非表示の切り替えができる星コンポーネント
    Signed-off-by: 辻忠頼 <103376179+TsujiTdayori@users.noreply.github.com>

---
+++ src/components/FavoriteStar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-icon color="#ffd803" size="large" v-if="favorite">mdi-star</v-icon>
+  <v-icon color="#ffd803" size="large" v-else>mdi-star-outline</v-icon>
+</template>
+
+<script>
+import {defineComponent} from "vue"
+
+export default defineComponent({
+  name: "FavoriteStar",
+  props:['favorite'],
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+</style>
